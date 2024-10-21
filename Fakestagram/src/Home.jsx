c40fac3876e59e1541df52d7e64dc64be83bd0ca
Bruno <brunitodito@gmail.com>
2024-10-21 10:21:07 -0300

    Cambios en el login

--- Fakestagram/src/Home.jsx
+++ Fakestagram/src/Home.jsx
@@ -4,21 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Fakestagram</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <label htmlFor='username'>User </label>
+        <input id='username' type='text' placeholder='' /> 
+        <label htmlFor='password'>Password </label>
+        <input type='text' placeholder='' />
+        <button>Login</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Create account here
       </p>
     </>
   )
