eecfff1a6e536d44db638f4ff2fac11939080eb3
Bruno <brunitodito@gmail.com>
2024-10-21 10:27:14 -0300

    Cambios en el login

--- Fakestagram/src/Home.jsx
+++ Fakestagram/src/Home.jsx
@@ -8,11 +8,13 @@ function App() {
   return (
     <>
       <h1>Fakestagram</h1>
-      <div className="card">
-        <label htmlFor='username'>User </label>
-        <input id='username' type='text' placeholder='' /> 
+      <label htmlFor='username'>User </label>
+      <input id='username' type='text' placeholder='' /> 
+      <div>
         <label htmlFor='password'>Password </label>
         <input type='text' placeholder='' />
+      </div>
+      <div>
         <button>Login</button>
       </div>
       <p>
